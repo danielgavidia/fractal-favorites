0b0939c7faabf8ba5ce26a164f0643443b012698
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-08 17:07:27 -0400

    Middleware changes

--- backend/express/middleware.ts
+++ backend/express/middleware.ts
@@ -2,12 +2,13 @@ import admin from "firebase-admin";
 import type { RequestHandler } from "express";
 
 // const serviceAccount = require("../firebase/credentials.json");
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
 const serviceAccount = {
 	type: process.env.FIREBASE_TYPE,
 	project_id: process.env.FIREBASE_PROJECT_ID,
 	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-	private_key: process.env.FIREBASE_PRIVATE_KEY,
+	private_key: privateKey,
 	client_email: process.env.FIREBASE_CLIENT_EMAIL,
 	client_id: process.env.FIREBASE_CLIENT_ID,
 	auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -16,9 +17,11 @@ const serviceAccount = {
 	client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
 };
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+if (!admin.apps.length) {
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+	});
+}
 
 export const verifyFirebaseToken: RequestHandler = async (req, res, next) => {
 	const idToken = req.headers.authorization?.split("Bearer ")[1];
